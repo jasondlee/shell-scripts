5af3dda4653fa38bde18ddd5cf7b26d8b237f3d8
Jason Lee <jason.lee@stablekernel.com>
2019-12-16 06:43:34 -0600

    Add slack Add restic Add restic-based script

--- new-system-setup.sh
+++ new-system-setup.sh
@@ -1,5 +1,5 @@
 # Enable RPM Fusion
-sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
      https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 # Enable OpenH264 repo
 sudo dnf config-manager --set-enabled fedora-cisco-openh264
@@ -9,17 +9,17 @@ sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.m
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 
 # Update the system
-sudo dnf update 
+sudo dnf update -y
 
 # Fix for slow startups with Intel SSDs
 sudo systemctl mask systemd-udev-settle
 
 # Install commonly used packages
 sudo dnf install -y kdiff3 thunderbird pidgin quassel-client the_silver_searcher git git-cola python3-pyside  \
-    python3-speedtest-cli vim-enhanced postgresql-server compat-ffmpeg28 \
-    libreoffice-calc libreoffice-writer akmod-nvidia xorg-x11-drv-nvidia-cuda dkms acpid \
-    ffmpeg-libs libatomic mc pidgin gstreamer1-libav gstreamer1-vaapi gstreamer1-plugins-{good,good-extras,ugly} \
-    gstreamer1-plugin-openh264 mozilla-openh264 jq gitk hplip hplip-gui youtube-dl mscore
+    python3-speedtest-cli vim-enhanced postgresql-server compat-ffmpeg28 libreoffice-calc libreoffice-writer \
+    akmod-nvidia xorg-x11-drv-nvidia-cuda dkms acpid ffmpeg-libs libatomic mc pidgin gstreamer1-libav \
+    gstreamer1-vaapi gstreamer1-plugins-{good,good-extras,ugly} gstreamer1-plugin-openh264 mozilla-openh264 jq \
+    gitk hplip hplip-gui youtube-dl mscore restic
 
 #sdkman
 curl -s "https://get.sdkman.io" | bash &&  source "$HOME/.sdkman/bin/sdkman-init.sh"
@@ -29,7 +29,7 @@ done
 
 # Google Drive support
 sudo dnf copr enable sergiomb/google-drive-ocamlfuse
-sudo dnf install google-drive-ocamlfuse
+sudo dnf install -y google-drive-ocamlfuse
 
 # Android emulator acceleration support
 sudo dnf group install --with-optional virtualization -y
@@ -44,3 +44,9 @@ mkdir -p ~/local/idrive/var/restore
 unzip IDriveForLinux.zip
 mv IDriveForLinux/scripts/* ~/local/idrive/bin/
 chmod +x ~/local/idrive/bin/*pl
+
+# Slack
+cd ~/Downloads
+wget https://downloads.slack-edge.com/linux_releases/slack-4.2.0-0.1.fc21.x86_64.rpm
+sudo dnf install -y ./slack*rpm
+rm slack*rpm

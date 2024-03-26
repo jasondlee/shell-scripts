5a9c4dbf9cae3c58d4d2e1e4535dfc5d4c0d5eb8
Jason Lee <jason@steeplesoft.com>
2024-03-26 12:54:03 -0500

    ShellCheck updates for startserver.sh

--- startserver.sh
+++ startserver.sh
@@ -2,128 +2,178 @@
 
 SUSPEND=n
 OTEL=false
-CLEAN=FALSE
+CLEAN=false
 BASE_DIR=~/src/redhat/wildfly/wildfly-full/build/target/
 CONFIG=standalone.xml
-STATS=
-PORT=14250
 DEBUG_LOGGING=false
 MICROMETER=false
 CLI=""
+DEBUG=false
+#STATS="-Dwildfly.undertow.statistics-enabled=false -Dwildfly.statistics-enabled=false -Dwildfly.undertow.active-request-statistics-enabled=false"
 
 function pause() {
-    read -p "Press enter..."
+    if [ "$DEBUG" == "true" ] ; then
+        read -r -p "Press enter..."
+    fi
+}
+
+function debug() {
+    if [ "$DEBUG" == "true" ] ; then
+        echo "$*"
+    fi
 }
 
 function clean() {
-    if [ -e $SERVER_DIR ] ; then
+    if [ -e "$SERVER_DIR" ] ; then
         echo "Deleting $SERVER_DIR"
-        rm -rf $SERVER_DIR
+        rm -rf "$SERVER_DIR"
+    else
+        echo "SERVER_DIR does not exist: $SERVER_DIR"
     fi
 
-    ARCHIVE=`find $BASE_DIR -maxdepth 1 -type f -name \*zip -o -name \*.tar.gz | grep -v 'src\.'`
+    ARCHIVE=$( find "$BASE_DIR" -maxdepth 1 -type f -name \*zip -o -name \*.tar.gz | grep -v 'src\.' )
 
-    case $ARCHIVE in
+    case "$ARCHIVE" in
         *zip )
             echo "Unzipping $ARCHIVE"
-            unzip $ARCHIVE -d $BASE_DIR &> /dev/null
+            unzip "$ARCHIVE" -d "$BASE_DIR" &> /dev/null
             ;;
         *.tar.gz )
             echo "Untarring $ARCHIVE"
-            tar xf $ARCHIVE -C $BASE_DIR
+            tar xf "$ARCHIVE" -C "$BASE_DIR"
             ;;
     esac
     find_server_dir
 }
 
 function find_server_dir() {
-    SERVER_DIR=`find $BASE_DIR -maxdepth 1 -type d -name wildfly\*`
+    SERVER_DIR=$( find "$BASE_DIR" -maxdepth 1 -type d -name wildfly\* )
 
     if [ "$SERVER_DIR" == "" ] ; then
-        SERVER_DIR=`find $BASE_DIR -maxdepth 1 -type d -name jboss-eap\*`
+        SERVER_DIR=$( find "$BASE_DIR" -maxdepth 1 -type d -name jboss-eap\* )
     fi
 }
 
 function add_cli_commands() {
+    debug "===== Adding $*"
     if [ "$CLI" == "" ]; then
-        CLI=`mktemp clicommandsXXX`
-        echo "embed-server -c=$CONFIG" > $CLI
+        CLI=$( mktemp clicommandsXXX )
+        debug "CLI file is $CLI"
+        echo "embed-server -c=$CONFIG" > "$CLI"
     fi
 
-    echo "$*" >> $CLI
+    echo "$*" >> "$CLI"
 }
 
 
-while getopts "cmMfnosSw:L" opt ; do
-    case $opt in
+while getopts "cmMfnosw:Ld" opt ; do
+    debug "=== opt = $opt"
+    case "$opt" in
+        L) debug "Setting logging level"
+            DEBUG_LOGGING=true
+            ;;
         c) CLEAN=true ;;
+        d) DEBUG=true ;;
         o) OTEL=true ;;
         m) CONFIG=standalone-microprofile.xml ;;
-        M) MICROMETER=true ;;
+        M) MICROMETER="true" ;;
         f) CONFIG=standalone-full.xml ;;
         s) SUSPEND=y ;;
-        S) STATS="-Dwildfly.undertow.statistics-enabled=true -Dwildfly.statistics-enabled=true -Dwildfly.undertow.active-request-statistics-enabled=true" ;;
+        #S) STATS=$( echo $STATS | sed -e 's/false/true/g') ;;
         n) DONTRUN="true" ;;
         w) BASE_DIR=$OPTARG ;;
-        L) DEBUG_LOGGING=true ;;
+        *) echo "Unrecognized option: $opt" ; exit 1 ;;
     esac
 done
+
+if [ "$DEBUG" == "true" ] ; then
+    set -x
+fi
 
 echo "Default SERVER_DIR = $SERVER_DIR"
 
 find_server_dir
 
+pause
+
 if [ "$CLEAN" == "true" ] ; then
     clean
 fi
+set +x
+
+pause
 
 echo "Discovered SERVER_DIR = $SERVER_DIR"
 
-if [ "$SERVER_DIR" == "" -o ! -e "$SERVER_DIR" ] ; then
+if [ "$SERVER_DIR" == "" ] || [ ! -e "$SERVER_DIR" ] ; then
     echo "Unable to determine server base directory"
-    exit -1
+    exit 1
 fi
 
 sed --in-place "s/#\?JAVA_OPTS=\"\$JAVA_OPTS -agentlib:jdwp=transport=dt_socket,address=8787,server=y,suspend=[ny]\"/JAVA_OPTS=\"\$JAVA_OPTS -agentlib:jdwp=transport=dt_socket,address=8787,server=y,suspend=$SUSPEND\"/" \
-    $SERVER_DIR/bin/standalone.conf
+    "$SERVER_DIR/bin/standalone.conf"
 
-if [ "$DEBUGG_LOGGING" == "true" ] ; then
+pause
+
+debug "DEBUG_LOGGING = $DEBUG_LOGGING"
+
+if [ "$DEBUG_LOGGING" == "true" ] ; then
     add_cli_commands "/subsystem=logging/console-handler=CONSOLE:write-attribute(name=level,value=DEBUG)
     /subsystem=logging/root-logger=ROOT:write-attribute(name=level,value=DEBUG)"
 fi
 
+
+pause
+
 if [ "$OTEL" == "true" ] ; then
-    add_cli_commands "if (outcome != success) of /extension=org.wildfly.extension.opentelemetry:read-resource
+    #add_cli_commands "if (outcome != success) of /extension=org.wildfly.extension.opentelemetry:read-resource
+    add_cli_commands << EOF
         /extension=org.wildfly.extension.opentelemetry:add()
         /subsystem=opentelemetry:add()
-    fi
-    /subsystem=opentelemetry:write-attribute(name=exporter-type,value=otlp)
-    /subsystem=opentelemetry:write-attribute(name=sampler-type,value=on)
-    /subsystem=opentelemetry:write-attribute(name=max-queue-size,value=128)
-    /subsystem=opentelemetry:write-attribute(name=max-export-batch-size,value=512)"
+
+        /subsystem=opentelemetry:write-attribute(name=sampler-type,value=on)
+        /subsystem=opentelemetry:write-attribute(name=batch-delay,value=1)
+        /subsystem=opentelemetry:write-attribute(name=max-queue-size,value=1)
+        /subsystem=opentelemetry:write-attribute(name=max-export-batch-size,value=512)
+EOF
 fi
 
 if [ "$MICROMETER" == "true" ] ; then
-    add_cli_commands "if (outcome != success) of /extension=org.wildfly.extension.micrometer:read-resource
+    #add_cli_commands "if (outcome != success) of /extension=org.wildfly.extension.micrometer:read-resource
+    #add_cli_commands " /extension=org.wildfly.extension.micrometer:add
+    #    /subsystem=micrometer:add(endpoint=\"http://localhost:4318/v1/metrics\")
+    #    /subsystem=micrometer:write-attribute(name=step,value=1)
+    #    /subsystem=undertow:write-attribute(name=statistics-enabled,value=true)"
+    add_cli_commands << EOF
+        if (outcome != success) of /extension=org.wildfly.extension.micrometer:read-resource
             /extension=org.wildfly.extension.micrometer:add
-            /subsystem=micrometer:add(endpoint=\"http://localhost:4317/v1/metrics\")
-        fi
-        /subsystem=micrometer:write-attribute(name=step,value=10)
-        /subsystem=undertow:write-attribute(name=statistics-enabled,value=true)"
+        end-if
+
+        if (outcome != success) of /subsystem=micrometer:read-resource
+            /subsystem=micrometer:add()
+            reload
+        end-if
+        /subsystem=micrometer/registry=otlp:add(endpoint="http://localhost:4318/v1/metrics")
+        /subsystem=micrometer/registry=otlp:write-attribute(name="step",value="1")
+EOF
 fi
 
 if [ "$CLI" != ""  ] ; then
     echo "Configuring the server..."
-    echo "reload" >> $CLI
-    $SERVER_DIR/bin/jboss-cli.sh --file=$CLI
-    rm $CLI
+    echo "reload" >> "$CLI"
+    "$SERVER_DIR"/bin/jboss-cli.sh --echo-command "--file=$CLI"
+    rm "$CLI"
 fi
 
-PATH=$SERVER_DIR/bin:$PATH
+pause
 
-JBOSS_HOME=$SERVER_DIR
-echo Starting WildFly from $SERVER_DIR
+PATH="$SERVER_DIR/bin:$PATH"
+
+export JBOSS_HOME="$SERVER_DIR"
+echo Starting WildFly from "$SERVER_DIR"
 
 if [ "$DONTRUN" != "true" ] ; then
-    $SERVER_DIR/bin/standalone.sh -c $CONFIG $STATS
+    "$SERVER_DIR"/bin/standalone.sh -c "$CONFIG"
 fi
+
+set +x
